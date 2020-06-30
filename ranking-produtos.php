ca3718eaf6b2b3857df23608ecf3384b2337f1d2
jp-caldeira <jpcm86@gmail.com>
2020-06-30 19:11:36 -0300

    começando página de ranking

---
+++ ranking-produtos.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ranking de Produtos</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="css\ranking.css">
+  </head>
+<body>
+  <header>
+      <nav class="navbar navbar-expand-lg navbar-dark" style='background-color: #03989e;'>
+          <div class='container-fluid'>
+              <a class="navbar-brand">Logo da empresa aqui</a>
+          <form class="form-inline col-9">
+              <input class="form-control col-10" type="search" placeholder="Digite aqui o que você quer encontrar (ex.: ração para gatos)">
+              <button class="btn btn-light" type="submit">Pesquisar</button>
+          </form>
+          <ul class='navbar-nav'>
+              <li class="nav-item">
+                  <a class="nav-link botao-1" href='#'>Login</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href='#'>Cadastro</a>
+              </li>
+          </ul>
+
+      </div>
+    </nav>
+  </header>
+
+<div class="container">
+  <h1>Ranking de Produtos</h1>
+</div>
+
+    <h2>Produto 1</h2>
+    <p>Imagem</p>
+    <p>Descrição</p>
+    <p>Preço</p>
+    <h2>Produto 2</h2>
+    <p>Imagem<p>
+    <p>Descrição</p>
+    <p>Preço</p>
+    <h2>Produto 3</h2>
+    <p>Imagem<p>
+    <p>Descrição</p>
+    <p>Preço</p>
+    <h2>Produto 4</h2>
+    <p>Imagem<p>
+    <p>Descrição</p>
+    <p>Preço</p>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
