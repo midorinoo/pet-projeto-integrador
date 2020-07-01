1da5e1e6acacace678c0a21c92d52ce88cdd267a
jp-caldeira <jpcm86@gmail.com>
2020-07-01 12:00:18 -0300

    add produtos na pagina ranking

--- ranking-produtos.php
+++ ranking-produtos.php
@@ -11,47 +11,78 @@
   <header>
       <nav class="navbar navbar-expand-lg navbar-dark" style='background-color: #03989e;'>
           <div class='container-fluid'>
-              <a class="navbar-brand">Logo da empresa aqui</a>
-          <form class="form-inline col-9">
+              <a class="navbar-brand col-2">Logo da empresa aqui</a>
+          <form class="form-inline col-8">
               <input class="form-control col-10" type="search" placeholder="Digite aqui o que você quer encontrar (ex.: ração para gatos)">
               <button class="btn btn-light" type="submit">Pesquisar</button>
           </form>
           <ul class='navbar-nav'>
               <li class="nav-item">
-                  <a class="nav-link botao-1" href='#'>Login</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href='#'>Cadastro</a>
+                  <a class="btn btn-light" href=#>Login</a>
               </li>
           </ul>
 
       </div>
     </nav>
   </header>
+<div class="site-content">
+        <div class="container titulo-principal">
+            <h1 class="display-4">Ranking de Produtos</h1>
+        </div>
+        <div class="produtos">
+            <div class="row">
+                  <div class="col-lg-5 offset-lg-1">
+                        <img src="img\racao2.jpg" class="img-fluid" alt="racao2">
+                        <h4><a href="#">Ração Magnus para cachorro 3kg</a></h4>
+                        <p>Ração indicada para cães de grande porte</p>
+                        <p>R$ 29,90</p>
+                        <a class='btn btn-danger btn-lg' href='#'>Comprar</a>
+                  </div>
+                  <div class="col-lg-5">
+                        <img src="img\racao1.jpg" class="img-fluid" alt="racao1">
+                        <h4><a href="#">Golden Formula carne e arroz 2kg</a></h4>
+                        <p>Feita para cães de pequeno porte</p>
+                        <p>R$ 39,90</p>
+                        <a class='btn btn-danger btn-lg' href='#'>Comprar</a>
+                  </div>
+                </div>
+                <br>
+                <div class="row">
+                      <div class="col-lg-5 offset-lg-1">
+                            <img src="img\racao3.jpg" class="img-fluid" alt="racao3">
+                            <h4><a href="#">Ração Magnus para cachorro 3kg</a></h4>
+                            <p>Ração indicada para cães de grande porte</p>
+                            <p>R$ 29,90</p>
+                            <a class='btn btn-danger btn-lg' href='#'>Comprar</a>
+                      </div>
+                      <div class="col-lg-5">
+                            <img src="img\racao4.jpg" class="img-fluid" alt="racao4">
+                            <h4><a href="#">Ração para cachorro 1kg</a></h4>
+                            <p>Feita para cães de pequeno porte</p>
+                            <p>R$ 39,90</p>
+                            <a class='btn btn-danger btn-lg' href='#'>Comprar</a>
+                      </div>
+                    </div>
+                    <br>
+                  <div class="row">
+                        <div class="col-lg-5 offset-lg-1">
+                            <img src="img\racao5.jpg" class="img-fluid" alt="racao5">
+                              <h4><a href="#">Ração Magnus para cachorro 3kg</a></h4>
+                              <p>Ração indicada para cães de grande porte</p>
+                              <p>R$ 29,90</p>
+                              <a class='btn btn-danger btn-lg' href='#'>Comprar</a>
+                          </div>
+                          <div class="col-lg-5">
+                                <img src="img\racao5.jpg" class="img-fluid" alt="racao5">
+                                <h4><a href="#">Golden Formula carne e arroz 2kg</a></h4>
+                                <p>Feita para cães de pequeno porte</p>
+                                <p>R$ 39,90</p>
+                                <a class='btn btn-danger btn-lg' href='#'>Comprar</a>
+                          </div>
+                        </div>
 
-<div class="container">
-  <h1>Ranking de Produtos</h1>
+        </div>
 </div>
-
-    <h2>Produto 1</h2>
-    <p>Imagem</p>
-    <p>Descrição</p>
-    <p>Preço</p>
-    <h2>Produto 2</h2>
-    <p>Imagem<p>
-    <p>Descrição</p>
-    <p>Preço</p>
-    <h2>Produto 3</h2>
-    <p>Imagem<p>
-    <p>Descrição</p>
-    <p>Preço</p>
-    <h2>Produto 4</h2>
-    <p>Imagem<p>
-    <p>Descrição</p>
-    <p>Preço</p>
-
-
-
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
